7ea03ea2857e875b54c76fb2255193966d67fd3e
Sandhyayadav77 <sandhyayadav0006@gmail.com>
2023-05-28 01:07:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import SocialLinks from './components/SocialLinks';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Experience from './components/Experience';
+import Contact from './components/Contact';
+
+function App() {
+  return (
+    <>
+    <Navbar/>
+    <Home/>
+    <SocialLinks/>
+    <About/>
+    <Portfolio/>
+    <Experience/>
+    <Contact/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/About.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <>
+            <div name='about' className='w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white '>
+                <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center  w-full h-full'>
+                    <div className='pb-8'>
+                        <p className='text-4xl font-bold inline border-b-4 border-gray-500'>
+                            About 
+                        </p>
+                    </div>
+
+                    <p className='text-xl pt-20  '>Lorem ipsum dolor sit amet consectetur adipisicing elit. A, veniam odio! Eveniet non quisquam nihil optio aspernatur fuga enim tenetur reiciendis sapiente at ea modi deserunt debitis consequuntur vitae alias officiis impedit repellat aliquid, hic expedita rerum. Deleniti tenetur, itaque officiis suscipit placeat odio nostrum assumenda eveniet quidem delectus mollitia?</p> <br />
+                    <p className='text-xl'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quo nam laborum. Alias, id doloribus? Quod suscipit repudiandae tempore. Deleniti, dolorem assumenda animi placeat necessitatibus praesentium adipisci aperiam repudiandae, officia sint suscipit quos qui doloribus odio libero cupiditate vel tempore unde perspiciatis. Provident voluptatum rerum rem accusantium fugiat soluta optio?
+                    </p>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default About
